a6b35b40156525db881006056c68ce13f3d1b4d2
Timothy Dillman <timothy.dillman@mobilefirst.me>
2022-12-10 11:36:36 -0600

    init commit

---
+++ index.js
@@ -0,0 +1,85 @@
+const express = require('express')
+const { download } = require('express/lib/response')
+const app = express()
+const PORT = process.env.PORT || 3000
+const router = express.Router()
+const axios = require('axios')
+const Fs = require('fs')
+const Path = require('path')
+const uuid = require('uuid')
+const { env } = require('process')
+const sharp = require('sharp')
+
+router.use((req, res, next) => {
+    loggingToConsole("==== DEBUG ROUTE ====")
+    loggingToConsole(req.hostname)
+    loggingToConsole(req.ip)
+    loggingToConsole(req.method)
+    loggingToConsole(req.params)
+    loggingToConsole(req.path)
+    next()
+})
+
+router.get('/', async (req, res) => {
+    const { width, height, url } = req.query
+    if(!width || !height || !url) { 
+        return res.status(400).send()
+    }
+    const file = await downloadURL(url)
+    const result = await resize(file, width, height)
+    await Fs.promises.unlink(file)
+    res.contentType('image/png').end(result)
+    
+})
+
+// function to download image file or attempt to download file
+async function downloadURL(url) {
+    const path = Path.resolve(__dirname, 'caches', `file-${uuid.v4()}`)
+    if (process.env.DEBUG) {
+        console.log("=== resolved path ===")
+        console.log(path)
+    }
+    return new Promise(async (resolve, reject) => {
+        axios({
+            method: 'GET',
+            url,
+            responseType: 'stream'
+        })
+        .then(response => {
+            response.data.pipe(Fs.createWriteStream(path))
+            response.data.on('end', () => {
+                resolve(path)
+            })
+            response.data.on('error', (err) => {
+                resolve(err)
+            })
+        })
+    })
+}
+
+async function resize(path, width, height) {
+    return new Promise((resolve, reject) => {
+        sharp(path)
+        .resize({
+            width: Number(width),
+            height: Number(height),
+            fit: sharp.fit.fit,
+            background: { r: 255, g: 255, b: 255, alpha: 1.0 }
+        })
+        .png()
+        .toBuffer()
+        .then(data => { resolve(data) })
+        .catch(err => { reject(err) })
+    })
+}
+
+function loggingToConsole(data) {
+    if (process.env.DEBUG) {
+        console.log(data)
+    }
+}
+
+app.use('/', router)
+app.listen(PORT, () => {
+    console.log(`Up and running on port ${PORT}`)
+})
